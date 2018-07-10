40573c4d1bce3962399d01127e60c311c7f83d37
Ogilo <alphaogilo@gmail.com>
2018-07-10 12:08:34 +0300

    Setup and add Basic Authentication

---
+++ authors/apps/authentication/backends.py
@@ -0,0 +1,10 @@
+# import jwt
+#
+# from django.conf import settings
+#
+# from rest_framework import authentication, exceptions
+#
+# from .models import User
+
+"""Configure JWT Here"""
+
---
+++ authors/apps/authentication/renderers.py
@@ -0,0 +1,25 @@
+import json
+
+from rest_framework.renderers import JSONRenderer
+
+
+class UserJSONRenderer(JSONRenderer):
+    charset = 'utf-8'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        # If the view throws an error (such as the user can't be authenticated
+        # or something similar), `data` will contain an `errors` key. We want
+        # the default JSONRenderer to handle rendering errors, so we need to
+        # check for this case.
+        errors = data.get('errors', None)
+
+        if errors is not None:
+            # As mentioned about, we will let the default JSONRenderer handle
+            # rendering errors.
+            return super(UserJSONRenderer, self).render(data)
+
+
+        # Finally, we can render our data under the "user" namespace.
+        return json.dumps({
+            'user': data
+        })
