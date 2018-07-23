5bf1239a07e2366a809e57637519fd7f44910579
Emmanuel Chepsire <manuh@Emmanuels-MacBook-Pro.local>
2018-07-23 17:48:42 +0300

    :arrow_upper_right: [Chore #159053953] Upgrade to django 2.0.6

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import (
     LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView
 )
-
+app_name = 'authentication'
 urlpatterns = [
     url(r'^user/?$', UserRetrieveUpdateAPIView.as_view()),
     url(r'^users/?$', RegistrationAPIView.as_view()),
